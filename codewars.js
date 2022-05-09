5f5a8327f9c478ad2f38e243caa374f9db7c719a
Levi Shevick <levidshevick@gmail.com>
2022-05-09 15:01:02 -0400

    solves 1st codewar

--- codewars.js
+++ codewars.js
@@ -2,8 +2,15 @@
 // Testing 1-2-3
 // https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/javascript
 
-
-
+var number=function(array){
+    if (array === []) {
+     return [];
+   } else {
+      return array.map((e, i) => {
+        return e = `${i + 1}: ${e}`;
+      })
+    }
+   };
 
 // # 2
 // Filling an array (part 1)
